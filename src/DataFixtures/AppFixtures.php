30f999769a10cc7a7fdaafa85591bada357ca64a
Samba <dllsamba01@gmail.com>
2022-12-22 12:34:04 +0100

    episode 7 fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()  
+    {
+        $this->faker = Factory::create('fr_FR');
+
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i=1; $i <= 50; $i++) { 
+            # code...
+            $ingredient = new Ingredient();
+            $ingredient ->setName($this->faker->word())
+            ->setPrice(mt_rand(0,100));
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+    }
+}
