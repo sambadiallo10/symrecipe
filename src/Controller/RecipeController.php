81bd2e4a4e3f21a757be47afec2e1deed3911dc3
Samba <dllsamba01@gmail.com>
2023-04-02 15:37:49 +0200

    init

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Recipe;
+
+
+class RecipeController extends AbstractController
+{
+    /**
+     * This controller display all recipes
+     *
+     * @param RecipeRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/recette', name: 'recipe.index', methods: ['GET'])]
+    public function index(RecipeRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $recipes = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10 
+        );  
+
+        return $this->render('pages/recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -26,17 +27,39 @@ class AppFixtures extends Fixture
     {
         
 
-        // $product = new Product();
-        // $manager->persist($product);
-
+        
+        //Ingredients
+        $ingredients = [];
         for ($i=1; $i <= 50; $i++) { 
             # code...
             $ingredient = new Ingredient();
             $ingredient ->setName($this->faker->word())
             ->setPrice(mt_rand(0,100));
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
 
+        //Recettes
+        for($j=0; $j< 25; $j++){
+            $recipe = new Recipe();
+            $recipe->setName(($this->faker->word()))
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                ->setDescription($this->faker->text(300))
+                ->setPrix(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
+
+                for ($k = 0; $k < mt_rand(5, 15); $k++){
+                    $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) -1)]);
+
+                }
+
+                $manager->persist($recipe);
+
+        }
+
+        
         $manager->flush();
     }
 }
